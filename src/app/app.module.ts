4b64c802dd3b8a8cae8079f4d3e138c35e0e57d3
njabulo.thwala <njabulo.thwala@symbiotics.co.za>
2017-10-06 01:42:12 +0200

    "Importing RegisterComponent"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,27 @@ import { NgModule } from '@angular/core';
 import { Http, HttpModule } from '@angular/http';
 import { Observable } from 'rxjs/Rx';
 import { AppComponent } from './app.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { ProfileComponent } from './api/component/profile/profile.component';
 import { ChatComponent } from './api/component/chat/chat.component';
 import { LoginComponent } from './api/component/login/login.component';
 import { Routing } from './router';
-
+import { RegisterComponent} from './api/component/register/register.component';
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     ChatComponent,
-    ProfileComponent
+    ProfileComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     HttpModule,
     FormsModule,
-    Routing
+    Routing,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
